21886ad6f324269b04f74cdc8aa19fec597dc5b9
AkShAy21211 <114931565+AkShAy21211@users.noreply.github.com>
2024-03-25 16:14:39 +0530

    Delete src/constants/constants.js

--- src/constants/constants.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'https://api.themoviedb.org/3';
-export const API_KEY = '584baf6f0ccecd3adfa4b61ed088077c';
-export const number = Math.floor(Math.random()*20);
-export const IMG_URL = 'https://image.tmdb.org/t/p/original';
